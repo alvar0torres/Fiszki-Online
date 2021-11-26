5910ed66f232a327ea2b9cd00dc90a3933617784
Alvaro <995.alvaro@gmail.com>
2021-11-26 21:15:57 +0100

    added google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-1KW0X0YW2V");
+ReactGA.send("pageview");
+
+
 
 ReactDOM.render(
   <React.StrictMode>
